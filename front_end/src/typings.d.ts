9f9e28cb46349bf3e32ea5ba01549be1c4cfcd2b
imran-tag <132948191+imran-tag@users.noreply.github.com>
2026-02-23 15:41:16 +0100

    Delete front_end directory

--- front_end/src/typings.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'bootstrap' {
-  export class Modal {
-    constructor(element: HTMLElement, options?: any);
-    show(): void;
-    hide(): void;
-    toggle(): void;
-    static getInstance(element: HTMLElement): Modal | null;
-  }
-}
